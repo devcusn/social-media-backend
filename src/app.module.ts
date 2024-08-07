f882b46e526082efa0023f16a37debd75edbb586
Halil Tunc <hitunc@outlook.com>
2024-08-07 20:42:20 +0300

    feat: auth files move to parent dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth/auth.controller';
-import { AuthService } from './auth/auth/auth.service';
-import { AuthModule } from './auth/auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './core/prisma/prisma.service';
 
 @Module({
