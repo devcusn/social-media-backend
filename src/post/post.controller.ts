b57da8f9db8250f56f6ee01c839db94765eed948
Halil Tunc <hitunc@outlook.com>
2024-08-19 14:38:53 +0300

    feat: image upload service add

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { Public } from 'src/_core/guards/auth.guard';
 
-@Controller('post')
-export class PostController {}
+@Controller('posts')
+export class PostController {
+  @Public()
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadImage(@UploadedFile() file) {
+    console.log('file upload', file);
+  }
+
+  @Public()
+  @Get('image/:filename')
+  async getImage(@Param('filename') filename: string, @Res() res: Response) {
+    const imagePath = join(__dirname, '..', '..', 'uploads', filename);
+    res.sendFile(imagePath);
+  }
+  @Public()
+  @Get('all')
+  async getAllImages() {
+    const files = readdirSync('./uploads');
+    const imageUrls = files.map((file) => ({
+      filename: file,
+      url: `http://localhost:4001/v1/posts/image/${file}`,
+    }));
+    return {
+      data: {
+        images: imageUrls,
+      },
+    };
+  }
+}
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   providers: [PostService],
   controllers: [PostController],
 })
